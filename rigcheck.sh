624e6a26496aae9ce6014ca2e09ccb6ece45208c
MrSamorus <33283925+MrSamorus@users.noreply.github.com>
2018-01-03 17:12:10 +0200

    Update rigcheck.sh

--- rigcheck.sh
+++ rigcheck.sh
@@ -4,8 +4,10 @@ apikey=483833928:AAEW7ADJeX8UeWZB0k9YzwIZC_dxj_wnKGY
 rigname=`grep loc /var/run/ethos/stats.file | sed 's/\(^\)\(.*\)loc:\s*\(.*\)/\1\3/'`
 xid=`dmesg -T -c | grep "NVRM: Xid" | grep -oE 'NVRM: Xid \(PCI:[^)]+\)' | cut -d':' -f4 | uniq | paste -sd','`
 defunct=`grep defunct /var/run/ethos/stats.file | sed 's/\(^\)\(.*\)defunct:\s*\(.*\)/\1\3/'`
+nomine=`cat /var/run/ethos/nomine.file`
 dissallowed=`grep allowed /var/run/ethos/stats.file | sed 's/\(^\)\(.*\)allowed:\s*\(.*\)/\1\3/'`
 overheat=`grep overheat /var/run/ethos/stats.file | sed 's/\(^\)\(.*\)overheat:\s*\(.*\)/\1\3/'`
+overheatedgpu=`cat /var/run/ethos/overheatedgpu.file`
 throttled=`grep throttled /var/run/ethos/stats.file | sed 's/\(^\)\(.*\)throttled:\s*\(.*\)/\1\3/'`
 
 
@@ -17,6 +19,14 @@ if [ "$defunct" -eq 1 ]
 		/opt/ethos/bin/clear-thermals
 		exit 1
 	fi
+
+if [ "$nomine" -eq 1 ]
+	then
+		/usr/bin/curl -m 5 -s -X POST --output /dev/null https://api.telegram.org/bot${apikey}/sendMessage -d text="${rigname} do not mining... Using clear-thermals. Failed cards: ${xid}" -d chat_id=${chatid}
+		/opt/ethos/bin/clear-thermals
+		exit 1
+	fi
+
 		
 if [ "$allowed" -eq 0 ]
 	then
@@ -30,7 +40,7 @@ if [ "$allowed" -eq 0 ]
 
 if [ "$overheat" -eq 1 ]
 	then
-		/usr/bin/curl -m 5 -s -X POST --output /dev/null https://api.telegram.org/bot${apikey}/sendMessage -d text="${rigname} overheated! Using clear-thermals." -d chat_id=${chatid}
+		/usr/bin/curl -m 5 -s -X POST --output /dev/null https://api.telegram.org/bot${apikey}/sendMessage -d text="${rigname} overheated! Overheated GPU: ${overheatedgpu} Using clear-thermals." -d chat_id=${chatid}
 		/opt/ethos/bin/clear-thermals
 		exit 1
 	fi
